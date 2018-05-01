a175522cd1a8f08b1c04bc89e1f007c278e14457
Greg Bunce <gbunce@utah.gov>
2018-05-01 10:42:01 -0600

    add ramps to query

--- non_spatial/Get_InterstatesAndRamps.sql
+++ non_spatial/Get_InterstatesAndRamps.sql
@@ -1,7 +1,7 @@
 -- these sql queries can run in ArcMap
 
--- return all interstates and rammps
-DOT_RTNAME like '0%' and (DOT_RTNAME like '%N' or DOT_RTNAME like '%P') and ONEWAY in (1, 2)
+-- return all interstates and ramps
+DOT_RTNAME like '0%' and (DOT_RTNAME like '%N%' or DOT_RTNAME like '%P%') and ONEWAY in (1, 2)
 
 -- return all interstates and ramps that have the correct oneway and milepost assignments
 (ONEWAY = 1 and DOT_F_MILE < DOT_T_MILE and DOT_RTNAME like '0%' and DOT_RTNAME like '%P%' and not DOT_RTNAME is null) 
