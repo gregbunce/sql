9f5c616a6e1a5a7163620950aba414da2f0f36b7
Greg Bunce <gbunce@utah.gov>
2018-05-02 14:59:57 -0600

    reword comment

--- non_spatial/Get_InterstatesAndRamps.sql
+++ non_spatial/Get_InterstatesAndRamps.sql
@@ -3,7 +3,7 @@
 -- return all interstates and ramps
 DOT_RTNAME like '0%' and (DOT_RTNAME like '%N%' or DOT_RTNAME like '%P%') and ONEWAY in (1, 2)
 
--- return all interstates and ramps that have the correct oneway and milepost assignments
+-- return all interstates (not ramps that have the correct oneway and milepost assignments
 (ONEWAY = 1 and DOT_F_MILE < DOT_T_MILE and DOT_RTNAME like '0%' and DOT_RTNAME like '%P%' and not DOT_RTNAME is null) 
 or 
 (ONEWAY = 2 and DOT_F_MILE < DOT_T_MILE and DOT_RTNAME like '0%' and DOT_RTNAME like '%N%' and not DOT_RTNAME is null) 
