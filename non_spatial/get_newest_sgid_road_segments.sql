a52c91947243af9a5c47e2832b547359da6ad826
Greg Bunce <gbunce@utah.gov>
2022-12-12 12:37:01 -0700

    fix address range, add create date

--- non_spatial/get_newest_sgid_road_segments.sql
+++ non_spatial/get_newest_sgid_road_segments.sql
@@ -1,8 +1,9 @@
 -- use this script to get the newest roads in the sgid roads feature class.
+
 declare @twoweeksago datetime
 declare @now datetime
 set @now = GETDATE()
 set @twoweeksago = DATEADD(day, -14, @now) 
 
-select FROMADDR_L, FROMADDR_R, PREDIR, FULLNAME, ADDRSYS_L, ZIPCODE_L  from Transportation.ROADS r 
+select FROMADDR_L, TOADDR_L, PREDIR, FULLNAME, ADDRSYS_L, ZIPCODE_L, CREATED from Transportation.ROADS r 
 where CREATED between @twoweeksago and @now 
